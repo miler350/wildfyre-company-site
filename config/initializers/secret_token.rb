ca4fed235dbe8b6bdd4cfbd88fa27fd9800de5ba
Cordis Stanfield <cordis.stanfield@gmail.com>
2013-08-06 23:44:49 -0400

    Initial commit, mysql setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WildfyreSite::Application.config.secret_key_base = '8c381310ece61c04cc4180876abdc4e6ac74825750a56795d66554555e247f9e569b9d6308db65eda1bf4392feca3b7af50fca84e7d84ca12f198ddcf7eeb4f9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WildfyreSite::Application.config.session_store :cookie_store, key: '_wildfyre-site_session'
