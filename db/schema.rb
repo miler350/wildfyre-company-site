958d7cdd7ab9d76c8800ad732651856766439b83
Cordis Stanfield <cordis.stanfield@gmail.com>
2013-08-07 10:37:23 -0400

    migrate heroku, add model/controller for contacts and projects

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130807045955) do
+ActiveRecord::Schema.define(version: 20130807143520) do
+
+  create_table "contacts", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "projects", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
