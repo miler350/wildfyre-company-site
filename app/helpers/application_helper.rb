3ae9e95f542e3b813d09adc820931e8595c5f082
Cordis Stanfield <cordis.stanfield@gmail.com>
2013-08-08 15:39:44 -0400

    user edit page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  
+  def abbreviate(string)
+    collection = []
+    string.split(" ").each do |word|
+      collection << word[0].capitalize 
+    end
+    collection.join
+  end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -2,12 +2,5 @@ class Project < ActiveRecord::Base
   has_many :relationships
   has_many :users, through: :relationships
   
-  def self.abbreviate
-    split = self.split(" ")
-    split.each do |word|
-      collection = []
-      collection << word[0].capitalize 
-      collection.join
-    end
-  end
+ 
 end
