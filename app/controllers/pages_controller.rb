e746f14e1dba55ed8107a57c9967329c4971ab24
Cordis Stanfield <cordis.stanfield@gmail.com>
2013-09-01 13:56:15 -0400

    set up some static pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,29 @@
+class PagesController < ApplicationController
+  
+  def companies
+  end
+  
+  def start_up
+  end
+  
+  def ventures
+  end
+  
+  def advisory
+  end
+  
+  def real_estate
+  end
+  
+  def projects
+  end
+  
+  def partners
+  end
+  
+  def network
+  end
+  
+  def ethos
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 WildfyreSite::Application.routes.draw do
+  # all static pages -------------------------
+  get '/companies' => 'pages#companies'
+  get '/projects' => 'pages#projects'
+  get '/partners' => 'pages#partners'
+  get '/network' => 'pages#network'
+  get '/ethos' => 'pages#ethos'
+  get '/start_up' => 'pages#start_up'
+  get '/ventures' => 'pages#ventures'
+  get '/advisory' => 'pages#advisory'
+  get '/real_estate' => 'pages#real_estate'
+  # end of static pages ----------------------
   get "uploads/index"
   get "uploads/create"
   get "uploads/destroy"
