aacfbe2243ffb6626553b93050c014850a65a2fb
Cordis Stanfield <cordis.stanfield@gmail.com>
2013-09-30 22:46:33 -0400

    back to reg

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,5 @@ WildfyreSite::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
   
-  config.assets.prefix = "/assets"
+ 
 end
