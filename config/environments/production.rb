d6029c899db90582f81e95cbc4abe93c2676d4db
Cordis Stanfield <cordis.stanfield@gmail.com>
2013-09-30 20:56:12 -0400

    add some production prefix config to test AP

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,6 @@ WildfyreSite::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  
+  config.assets.prefix = "/assets"
 end
